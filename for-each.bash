9fb41ea0ae9eff525a5d9e2952e071468d5b18d7
Quan Zhuo <zhuo.quan@outlook.com>
2016-07-25 18:40:47 +0800

    just worked

--- for-each.bash
+++ for-each.bash
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+function do_something() {
+    echo "$1"
+}
+
+function for_each() {
+#    echo "$PWD"
+    for var in $(ls $1)
+    do
+	if test -d $1/$var
+	then
+	    echo "$var is a directory"
+	    for_each $1/$var
+	else
+    	    do_something $1/$var
+	fi
+    done
+}
+
+for_each $1
