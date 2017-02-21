d56d8929597cc6a9de225bc9500c1689c683473a
Quan Zhuo <zhuo.quan@outlook.com>
2017-02-21 10:02:58 +0800

    sync parallel

--- sync-and-merge.sh
+++ sync-and-merge.sh
@@ -79,6 +79,14 @@ rsync -av --exclude=.repo/ --exclude=.git/ --exclude=prevHEAD \
       > $LOGDIR/rsync-$(date "+%Y-%m-%d") \
       2 > $LOGDIR/rsync-error-$(date "+%Y-%m-%d")
 
+# we can use the following commands to speed up
+# rsync -av --exclude=.git/ --exclude=prevHEAD --exclude=.gitignore -c -i --no-times --no-perms  --delete d1c/LINUX/android/external/ ple/LINUX/android/external/
+# rsync -av --exclude=.git/ --exclude=prevHEAD --exclude=.gitignore -c -i --no-times --no-perms  --delete d1c/LINUX/android/system/ ple/LINUX/android/system/
+# rsync -av --exclude=.git/ --exclude=prevHEAD --exclude=.gitignore -c -i --no-times --no-perms  --delete d1c/LINUX/android/packages/ ple/LINUX/android/packages/
+# rsync -av --exclude=.git/ --exclude=prevHEAD --exclude=.gitignore -c -i --no-times --no-perms  --delete d1c/LINUX/android/vendor/ ple/LINUX/android/vendor/
+# rsync -av --exclude=.git/ --exclude=prevHEAD --exclude=.gitignore --exclude=external/ --exclude=system/ --exclude=packages/ --exclude=vendor/ -c -i --no-times --no-perms --delete d1c/LINUX/ ple/LINUX/
+# rsync -av --exclude=.repo/ --exclude=.git/ --exclude=prevHEAD --exclude=.gitignore --exclude=LINUX/ --exclude=MPSS.TA.2.3/ --exclude=MSM8940/ -c -i --no-times --no-perms --delete d1c/ ple/
+
 if [ $? -eq 0 ];then
     date | tr '\n' ' ' >> $LOGFILE
     echo "--> merge successfully" >> $LOGFILE
@@ -86,3 +94,4 @@ else
     date | tr '\n' ' ' >> $LOGFILE
     echo "--> merge failed" >> $LOGFILE
 fi
+
